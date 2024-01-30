ee8fc99c51ec0d685e187fd6d346a1e337c2ff92
Mattia Ballico <mattia.ballico@gmail.com>
2024-01-30 14:50:52 +0100

    moved unused libraries to 'deprecated' directory

--- utility/__init__.py
+++ utility/__init__.py
@@ -1,5 +1,4 @@
 from utility.vectors2D import Vector2D, Vec2D_from_polar
 from utility.mycostants import Constants as const
 from utility.bodyclass import Body
-from utility.decorators import timer_func, memoize
 # ? this shit don't work properly
